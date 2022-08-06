4b3e80093108515a075a5f9295e6d295d249f899
Renuka4351 <renukakawde19@gmailcom>
2022-08-06 21:36:56 +0530

    add a program

---
+++ day8q2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -n "Enter the birthday (mm-dd-yyyy): "
+read bdate
+
+bmonth=${bdate:0:2}
+bday=${bdate:3:2}
+byear=${bdate:6:4}
+
+cdate='date + %m - %d - %y'
+cmonth=${cdate:0:2}
+cday=${cdate:3:2}
+cyear=${cdate:6:4}
+
+if [[ "$cmonth" -lt "$bmonth" ]] || [[ "$cmonth" -eq "$bmonth" && "$cday" -lt "$bday" ]]
+then
+  let age= cyear-byear-1
+else
+  let age= cyear-byear
+fi
+
+echo "Age = "$age"
